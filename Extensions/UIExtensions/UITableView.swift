7aa96e59f557f0d2d0ebc881371bde2c285c1be0
Severyn-Wsevolod <katolyk.severyn@gmail.com>
2019-07-05 14:09:13 +0300

    Add UI extensions

--- Extensions/UIExtensions/UITableView.swift
+++ Extensions/UIExtensions/UITableView.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 extension UITableView {
+    func register<T: UITableViewCell>(cell: T.Type) {
+        register(T.nib, forCellReuseIdentifier: T.identifier)
+    }
+    
+    func register<T: UIView>(reuseView: T.Type) {
+        register(T.nib, forHeaderFooterViewReuseIdentifier: T.identifier)
+    }
+    
     func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withIdentifier: T.identifier, for: indexPath) as? T else {
             fatalError("Could not find table view cell with identifier \(T.identifier)")
@@ -16,7 +24,21 @@ extension UITableView {
         return cell
     }
     
-    func register<T: UITableViewCell>(cell: T.Type) {
-        register(T.nib, forCellReuseIdentifier: T.identifier)
+    func dequeueReusableView<T: UIView>() -> T {
+        guard let view = dequeueReusableHeaderFooterView(withIdentifier: T.identifier) as? T else {
+            fatalError("Could not find table view cell with identifier \(T.identifier)")
+        }
+        return view
+    }
+    
+    func safeScrollToRow(at indexPath: IndexPath, at scrollPosition: UITableView.ScrollPosition, animated: Bool) {
+        guard dataSource?.tableView(self, cellForRowAt: indexPath) != nil else {
+            return
+        }
+        scrollToRow(at: indexPath, at: scrollPosition, animated: animated)
+    }
+    
+    func scrollToFirstRow(at scrollPosition: UITableView.ScrollPosition, animated: Bool) {
+        safeScrollToRow(at: .zero, at: scrollPosition, animated: animated)
     }
 }
